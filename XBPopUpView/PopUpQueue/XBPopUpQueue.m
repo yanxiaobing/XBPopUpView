3e73be9079ee5117e6f65b6b22e7f2d915c52ea4
xbingo <dove025@qq.com>
2019-11-01 17:57:38 +0800

    添加注释

--- XBPopUpView/PopUpQueue/XBPopUpQueue.m
+++ XBPopUpView/PopUpQueue/XBPopUpQueue.m
@@ -35,23 +35,43 @@
 }
 
 - (void)addView:(id<XBPopUpDelegate>)popUpView {
-        if ([self.popUpQueue containsObject:popUpView]) {
-            return;
+    if ([self.popUpQueue containsObject:popUpView]) {
+        return;
+    }
+    // 将弹窗添加到队列
+    [self.popUpQueue addObject:popUpView];
+    // 队列中只有一个弹窗
+    if (self.popUpQueue.count == 1) {
+        // 展示弹窗，如果lowerPriorityHidden == YES，则赋值给currentPopUpView
+        [self showAndRecordPopUpViewIfNeed:popUpView];
+    }
+    // 队列中有多个弹窗
+    else {
+        // 根据优先级对弹窗进行排序
+        [self sortPopUpQueue];
+        // 队列中有多个弹窗表示目前肯定有弹窗展示着
+        // 如果当前展示的弹窗支持临时隐藏，且当前弹窗非当前队列中第一个弹窗（优先级最高最先添加进来的）
+        if (self.currentPopUpView.lowerPriorityHidden && self.currentPopUpView != self.popUpQueue.firstObject) {
+            __weak typeof(self) weakSelf = self;
+            if ([self.currentPopUpView respondsToSelector:@selector(temporarilyDismissAnimated:completion:)]) {
+                [self.currentPopUpView temporarilyDismissAnimated:NO completion:^{
+                    [weakSelf showAndRecordPopUpViewIfNeed:weakSelf.popUpQueue.firstObject];
+                }];
+            }
         }
-        [self.popUpQueue addObject:popUpView];
-        if (self.popUpQueue.count == 1) {
-            [self showAndRecordPopUpViewIfNeed:popUpView];
-        } else {
-            
-            [self sortPopUpQueue];
-            
-            if (self.currentPopUpView.lowerPriorityHidden && self.currentPopUpView != self.popUpQueue.firstObject) {
-                __weak typeof(self) weakSelf = self;
-                if ([self.currentPopUpView respondsToSelector:@selector(temporarilyDismissAnimated:completion:)]) {
-                    [self.currentPopUpView temporarilyDismissAnimated:NO completion:^{
-                        [weakSelf showAndRecordPopUpViewIfNeed:weakSelf.popUpQueue.firstObject];
-                    }];
+    }
+}
+
+- (void)removeView:(id<XBPopUpDelegate>)popUpView {
+        if ([self.popUpQueue containsObject:popUpView]) {
+            [self.popUpQueue removeObject:popUpView];
+            if (self.popUpQueue.count > 0) {
+                id<XBPopUpDelegate> popUpView = self.popUpQueue.firstObject;
+                if ([popUpView respondsToSelector:@selector(present)]) {
+                    [popUpView present];
                 }
+            }else{
+                self.currentPopUpView = nil;
             }
         }
 }
@@ -76,20 +96,6 @@
         }
         return NSOrderedSame;
     }];
-}
-
-- (void)removeView:(id<XBPopUpDelegate>)popUpView {
-        if ([self.popUpQueue containsObject:popUpView]) {
-            [self.popUpQueue removeObject:popUpView];
-            if (self.popUpQueue.count > 0) {
-                id<XBPopUpDelegate> popUpView = self.popUpQueue.firstObject;
-                if ([popUpView respondsToSelector:@selector(present)]) {
-                    [popUpView present];
-                }
-            }else{
-                self.currentPopUpView = nil;
-            }
-        }
 }
 
 @end
